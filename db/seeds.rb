f29ad28a84ad8e3fb26c98a56404fd412601c8db
Farid <farid.babazov@gmail.com>
2015-11-07 12:27:53 +0600

    Generated User model used Devise, Added fixture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = []
+5.times do
+  name = Faker::Name.name
+  users.push User.create(name: name, email: Faker::Internet.email(name), password: '12345678', password_confirmation: '12345678')
+end
